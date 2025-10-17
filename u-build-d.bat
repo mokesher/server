7f3febec991257d0c3bfc552720ef340f07f9328
wangwenbin <wangwenbin@tgstor.cn>
2025-10-17 15:46:10 +0800

    aa

--- u-build-d.bat
+++ u-build-d.bat
@@ -1,11 +1,8 @@
 @echo off
 chcp 65001
-echo ==========================
-echo 开始安装依赖...
-echo ==========================
-call npm install
 
-echo 依赖安装完成，准备构建...
-call npm run build
-echo 构建完成！
+yarn install
+yarn build
+npm install node-windows --save
+
 pause
