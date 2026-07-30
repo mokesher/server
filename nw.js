f0c866a708637190abb5c76e1dc75d645e43a901
wangwenbin <wangwenbin@tgstor.cn>
2026-07-30 10:20:11 +0800

    aa

--- nw.js
+++ nw.js
@@ -5,23 +5,31 @@ const svc = new Service({
     description: '点亮网易云音乐灰色歌曲',
     script: './app.js', // 入口文件路径
     // scriptOptions:'-p 52100:52101', // 可选参数示例: 自定义端口并开启HTTPS
-    scriptOptions: '-o pyncmd kuwo bodian kugou migu ytdlp bilibili',
+    scriptOptions: '-o pyncmd kuwo bodian kugou migu bilibili',
     wait: '1', // 程序崩溃后重启时间间隔
     grow: '0.25', // 重启等待时间成长值，第一次1秒，第二次1.25秒。。。
     maxRestarts: '40', // 60秒内最大重启次数
     env: [
-        // {
-        //     name: 'ENABLE_FLAC',
-        //     value: 'true',
-        // },
+        {
+            name: 'ENABLE_FLAC',
+            value: 'true',
+        },
         {
             name: 'DISABLE_UPGRADE_CHECK',
-            value: 'DISABLE_UPGRADE_CHECK=true',
+            value: 'true',
         },
         // {
-        //     name: 'FOLLOW_SOURCE_ORDER',
-        //     value: 'FOLLOW_SOURCE_ORDER=true',
-        // }
+        //     name: 'LOG_LEVEL',
+        //     value: 'debug',
+        // },
+        // {
+        //     name: 'LOG_FILE',
+        //     value: 'app.log',
+        // },
+        {
+            name: 'FOLLOW_SOURCE_ORDER',
+            value: 'true',
+        }
     ],
 });
 
